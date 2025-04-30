e6cae935e932e1c6b8f7899be7a3bc219750e2c8
shanvisoni <shanvisoni7388@gmail.com>
2025-04-30 22:46:53 +0530

    correction db

--- config/db.js
+++ config/db.js
@@ -10,10 +10,7 @@ const pool = mysql.createPool({
   database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  queueLimit: 0
 });
 
 export default pool;
