c763fe2614df89a59564370de432e45f6ca40daa
shanvisoni <shanvisoni7388@gmail.com>
2025-04-30 20:23:12 +0530

    db corrections

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
